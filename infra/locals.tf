e531cda6e8214119585e05ce15a177b4aef76467
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-04-15 20:21:18 -0300

    teste

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,7 +1,5 @@
 locals {
-  glue_src_path = "${path.root}/../app"
-}
-
-locals {
+  glue_src_path  = "${path.root}/../app"
   glue_role_name = var.create_role ? aws_iam_role.glue_job_role[0].name : data.aws_iam_role.glue_job_role[0].name
+  glue_role_arn  = var.create_role ? aws_iam_role.glue_job_role[0].arn : data.aws_iam_role.glue_job_role[0].arn
 }
