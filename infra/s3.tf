e87d95d24121a287d7fcda75e815ed4f17c4b914
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-04-17 12:53:15 -0300

    teste

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,8 @@
+# Bucket auxiliar para códigos em python.
+resource "aws_s3_bucket" "bucket_aux" {
+  bucket = var.s3_bucket_aux
+}
+
 # Bucket principal para dados de entrada/saida processados pelo Glue.
 resource "aws_s3_bucket" "bucket_sot" {
   bucket = var.s3_bucket_sot
