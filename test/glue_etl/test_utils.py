e00f53d6f2c0c0cabaff93f562707d564547458e
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-04-26 14:42:16 -0300

    teste

--- test/glue_etl/test_utils.py
+++ test/glue_etl/test_utils.py
@@ -1,3 +1,36 @@
+    @patch("app.glue_etl.src.utils.wr")
+    def test_sem_particao_nao_adiciona_colunas(self, mock_wr):
+        df = self._df_base()
+        mock_wr.s3.read_json.return_value = df
+
+        resultado = processar_tmdb(
+            input_path="s3://bucket-sor/",
+            output_path="s3://bucket-sot/",
+            database="tmdb_dev",
+            table="movies_sot",
+            partition_cols=None
+        )
+
+        kwargs = mock_wr.s3.to_parquet.call_args.kwargs
+        self.assertEqual(kwargs["partition_cols"], [])
+        self.assertNotIn("year", kwargs["df"].columns)
+        self.assertNotIn("month", kwargs["df"].columns)
+
+    @patch("app.glue_etl.src.utils.wr")
+    def test_particao_customizada(self, mock_wr):
+        df = self._df_base()
+        mock_wr.s3.read_json.return_value = df
+
+        resultado = processar_tmdb(
+            input_path="s3://bucket-sor/",
+            output_path="s3://bucket-sot/",
+            database="tmdb_dev",
+            table="movies_sot",
+            partition_cols=["custom"]
+        )
+
+        kwargs = mock_wr.s3.to_parquet.call_args.kwargs
+        self.assertEqual(kwargs["partition_cols"], ["custom"])
 """Testes unitarios das funcoes utilitarias do Glue ETL."""
 
 import unittest
