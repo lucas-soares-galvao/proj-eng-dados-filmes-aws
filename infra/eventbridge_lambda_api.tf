ada3c3b501fadcd6e5120d69144a989e15d560c6
Lucas Soares Galvão <lsgalvao1000@gmail.com>
2026-05-31 00:09:41 -0300

    teste

--- infra/eventbridge_lambda_api.tf
+++ infra/eventbridge_lambda_api.tf
@@ -3,13 +3,13 @@
 resource "aws_cloudwatch_event_rule" "lambda_api_movie" {
   name        = "lambda-api-movie-${var.env}"
   description = "Dispara a Lambda para filmes"
-  schedule_expression = "cron(50 02 * * ? *)" # Todos os dias as 02:50 UTC
+  schedule_expression = "cron(15 03 * * ? *)" # Todos os dias as 03:15 UTC
 }
 
 resource "aws_cloudwatch_event_rule" "lambda_api_tv" {
   name        = "lambda-api-tv-${var.env}"
   description = "Dispara a Lambda para series"
-  schedule_expression = "cron(05 03 * * ? *)" # Todos os dias as 03:05 UTC
+  schedule_expression = "cron(30 03 * * ? *)" # Todos os dias as 03:30 UTC
 }
 
 resource "aws_cloudwatch_event_target" "lambda_api_movie_target" {
